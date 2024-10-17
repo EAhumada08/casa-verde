2b8808658579eced6ac6bdb8a5eb4b4c82a7a940
EAhumada08 <emaahmc08@gmail.com>
2024-10-16 20:45:30 -0600

    corregir ruteo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,28 +5,20 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <main>
-      <h1>{{title}}</h1>
-      @if (isAdmin){
-        <button >Erase database</button>
-      } @else {
-        <p>You are not authorized</p>
-      }
-      <ul>
-      @for (ingredient of ingredientList; track ingredient.name){
-        <li>{{ingredient.quantitity}} - {{ingredient.name}}</li>
-      }
-      </ul>
-    </main>
+      <div class="logo-container">
+        <img [src]="imgLogo" alt="asd">
+      </div>
   `,
-  styles: ``
+  styles: `
+  .logo-container{
+
+  }
+
+  img{
+    width: 500px;
+  }
+  `
 })
 export class HomeComponent {
-  title = "Home";
-  isAdmin = false;
-  ingredientList = [
-    {name: 'noodles', quantitity: 1},
-    {name: 'miso broth', quantitity: 1},
-    {name: 'egg', quantitity: 2},
-  ];
+  imgLogo = 'images/logo.png';
 }
